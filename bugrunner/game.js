e89d726a3bbe1e1fcc374e8ee6522b0c43f5cd06
Yousuf Mohammad Amin <ymohammad18@gmail.com>
2024-05-27 20:03:38 +0600

    trying mobile controls

--- bugrunner/game.js
+++ bugrunner/game.js
@@ -14,20 +14,18 @@ function preload() {
     gameState.player = this.physics.add.sprite(225, 450, 'codey').setScale(.5);
     
     const platforms = this.physics.add.staticGroup();
-  
     platforms.create(225, 490, 'platform').setScale(1, .3).refreshBody();
-  
+    
     gameState.scoreText = this.add.text(195, 485, 'Score: 0', { fontSize: '15px', fill: '#000000' });
-  
+    
     gameState.player.setCollideWorldBounds(true);
-  
     this.physics.add.collider(gameState.player, platforms);
     
-      gameState.cursors = this.input.keyboard.createCursorKeys();
-  
+    gameState.cursors = this.input.keyboard.createCursorKeys();
+    
     const bugs = this.physics.add.group();
-  
-    function bugGen () {
+    
+    function bugGen() {
       const xCoord1 = Math.random() * 450;
       const xCoord2 = Math.random() * 450;
       const xCoord3 = Math.random() * 450;
@@ -35,19 +33,19 @@ function preload() {
       bugs.create(xCoord2, 10, 'bug2');
       bugs.create(xCoord3, 15, 'bug3');
     }
-  
+    
     const bugGenLoop = this.time.addEvent({
       delay: 200,
       callback: bugGen,
       callbackScope: this,
       loop: true,
     });
-  
+    
     this.physics.add.collider(bugs, platforms, function (bug) {
       bug.destroy();
       gameState.score += 10;
       gameState.scoreText.setText(`Score: ${gameState.score}`);
-    })
+    });
     
     this.physics.add.collider(gameState.player, bugs, () => {
       bugGenLoop.destroy();
@@ -55,23 +53,37 @@ function preload() {
       this.add.text(180, 250, 'Game Over', { fontSize: '15px', fill: '#000000' });
       this.add.text(152, 270, 'Click to Restart', { fontSize: '15px', fill: '#000000' });
       
-          // Add your code below:
-      this.input.on('pointerup', () =>{
+      // Add your code below:
+      this.input.on('pointerup', () => {
         gameState.score = 0;
-          this.scene.restart();
+        this.scene.restart();
       });
     });
+  
+    // Mobile touch controls
+    const leftZone = this.add.zone(0, 500, 225, 500).setOrigin(0, 1).setInteractive();
+    const rightZone = this.add.zone(225, 500, 225, 500).setOrigin(0, 1).setInteractive();
+  
+    leftZone.on('pointerdown', () => gameState.moveLeft = true);
+    leftZone.on('pointerup', () => gameState.moveLeft = false);
+    leftZone.on('pointerout', () => gameState.moveLeft = false);
+  
+    rightZone.on('pointerdown', () => gameState.moveRight = true);
+    rightZone.on('pointerup', () => gameState.moveRight = false);
+    rightZone.on('pointerout', () => gameState.moveRight = false);
   }
   
+  
   function update() {
-    if (gameState.cursors.left.isDown) {
+    if (gameState.cursors.left.isDown || gameState.moveLeft) {
       gameState.player.setVelocityX(-160);
-    } else if (gameState.cursors.right.isDown) {
+    } else if (gameState.cursors.right.isDown || gameState.moveRight) {
       gameState.player.setVelocityX(160);
     } else {
       gameState.player.setVelocityX(0);
     }
   }
+  
   
   const config = {
     type: Phaser.AUTO,
